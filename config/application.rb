2363ff3121ae83eb53f43a1695df198027d173c4
Ben Scofield <git@turrean.com>
2022-08-05 13:28:17 -0400

    Add packages to autoload and eager_load paths

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module Foodie
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.autoload_paths << "#{root}/packages"
+    config.eager_load_paths << "#{root}/packages"
   end
 end
